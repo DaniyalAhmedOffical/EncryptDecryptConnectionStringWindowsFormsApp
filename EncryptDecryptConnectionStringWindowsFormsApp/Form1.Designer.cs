31d7cd2aee7e608befdc9fcefc1c4c96204501eb
Daniyal <Daniyal@DESKTOP-UODOQ15>
2024-07-24 16:28:45 +0500

    Add project files.

---
+++ EncryptDecryptConnectionStringWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EncryptDecryptConnectionStringWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(197, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(197, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(197, 91);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(197, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(130, 160);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(278, 160);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input/Encrypted String:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output/Decrypted String:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EncryptDecryptConnectionStringWindowsFormsApp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System; // System namespace ka istemal ho raha hai jo basic functions provide karta hai
+using System.Security.Cryptography; // Cryptographic services ke liye namespace
+using System.Text; // Text encoding ke liye namespace
+using System.Windows.Forms; // Windows Forms Application ke liye namespace
+
+namespace EncryptDecryptConnectionStringWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        // Encryption key define kar rahe hain, key ki length 16, 24 ya 32 characters honi chahiye AES ke liye
+        private readonly string key = "32"; // "your-encryption-key"; 
+
+        public Form1()
+        {
+            InitializeComponent(); // Form ko initialize karta hai
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string connectionString = txtInput.Text; // User se input liya ja raha hai
+            string encryptedString = Encrypt(connectionString, key); // Input ko encrypt kar rahe hain
+            txtOutput.Text = encryptedString; // Encrypted string ko output textbox mein display kar rahe hain
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string encryptedString = txtInput.Text; // User se input liya ja raha hai (encrypted string)
+            string decryptedString = Decrypt(encryptedString, key); // Encrypted string ko decrypt kar rahe hain
+            txtOutput.Text = decryptedString; // Decrypted string ko output textbox mein display kar rahe hain
+        }
+
+        // Encrypt method ko define kar rahe hain
+        public static string Encrypt(string plainText, string key)
+        {
+            byte[] iv = new byte[16]; // Initialization vector (IV) 16 bytes ka
+            using (Aes aes = Aes.Create()) // AES object create kar rahe hain
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key); // Key ko UTF8 bytes mein convert kar rahe hain
+                aes.IV = iv; // IV set kar rahe hain
+
+                // Encryptor create kar rahe hain AES key aur IV se
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                // MemoryStream ka use kar ke data ko memory mein store kar rahe hain
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    // CryptoStream ka use kar rahe hain encryption ke liye
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // StreamWriter ka use kar rahe hain data likhne ke liye
+                        using (System.IO.StreamWriter sw = new System.IO.StreamWriter(cs))
+                        {
+                            sw.Write(plainText); // Plain text ko write kar rahe hain
+                        }
+                        return Convert.ToBase64String(ms.ToArray());
+                        // Encrypted bytes ko Base64 string mein convert kar ke return kar rahe hain
+                    }
+                }
+            }
+        }
+
+        // Decrypt method ko define kar rahe hain
+        public static string Decrypt(string cipherText, string key)
+        {
+            byte[] iv = new byte[16]; // Initialization vector (IV) 16 bytes ka
+            byte[] buffer = Convert.FromBase64String(cipherText); 
+            // Cipher text ko Base64 se bytes mein convert kar rahe hain
+
+            using (Aes aes = Aes.Create()) // AES object create kar rahe hain
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key); // Key ko UTF8 bytes mein convert kar rahe hain
+                aes.IV = iv; // IV set kar rahe hain
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV); // Decryptor create kar rahe hain AES key aur IV se
+
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream(buffer)) // MemoryStream ka use kar rahe hain encrypted data ko memory mein rakhne ke liye
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) // CryptoStream ka use kar rahe hain decryption ke liye
+                    {
+                        using (System.IO.StreamReader sr = new System.IO.StreamReader(cs)) // StreamReader ka use kar rahe hain decrypted data ko read karne ke liye
+                        {
+                            return sr.ReadToEnd(); // Decrypted text ko read karke return kar rahe hain
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
